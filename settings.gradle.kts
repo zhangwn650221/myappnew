74cbb7df23224fd8cecf7c486267c58a7612265d
@zhangwn650221 <zwn650221@outlook.com>
2025-06-28 22:22:53 +0800

    chore: 批量提交近期所有未提交的代码和配置修改

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,11 +14,19 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // 首先使用阿里云的镜像 (Kotlin 语法)
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+
+        // 保留官方的作为备用
         google()
         mavenCentral()
     }
 }
-
 rootProject.name = "My Appnew"
 include(":app")
  
